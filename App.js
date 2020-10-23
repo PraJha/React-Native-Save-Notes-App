a0eae679c983622575c081d1a1a62c52eeec9c85
PraJha <abcpr108@gmail.com>
2020-10-23 19:25:15 +0530

    Now User login will persist uesr will not have to login each time

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+
+import { firebase } from './src/firebase/config'
+
 import { decode, encode } from 'base-64'
 if (!global.btoa) { global.btoa = encode }
 if (!global.atob) { global.atob = decode }
@@ -13,6 +17,27 @@ export default function App() {
 
     const [loading, setLoading] = useState(true)
     const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const usersRef = firebase.firestore().collection('users');
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                usersRef
+                    .doc(user.uid)
+                    .get()
+                    .then((document) => {
+                        const userData = document.data()
+                        setLoading(false)
+                        setUser(userData)
+                    })
+                    .catch((error) => {
+                        setLoading(false)
+                    });
+            } else {
+                setLoading(false)
+            }
+        });
+    }, []);
 
     return (
         <NavigationContainer>
