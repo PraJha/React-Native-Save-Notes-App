86b27101c66908ff88b3aacb801bf6758aed31c1
PraJha <abcpr108@gmail.com>
2020-10-23 18:46:35 +0530

    Now the app is working, email authentication both registration and login is working. Also we are able to go to our homescreen where we can save our notes.

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import FirstPage from './containers/FirstPage'
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import { decode, encode } from 'base-64'
+if (!global.btoa) { global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState(null)
+
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen
-                    name="First"
-                    component={FirstPage}
-                />
+                {user ? (
+                    <Stack.Screen name="HomeScreen">
+                        {props => <HomeScreen {...props} extraData={user} />}
+                    </Stack.Screen>
+                ) : (
+                        <>
+                            <Stack.Screen name="Login" component={LoginScreen} />
+                            <Stack.Screen name="Registration" component={RegistrationScreen} />
+                            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                        </>
+                    )}
             </Stack.Navigator>
         </NavigationContainer>
     );
